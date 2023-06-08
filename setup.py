dffa8f154d2eb033ca0c5cc7a7bc46b377e13178
Pedro Ribeiro <pedroeusebio@gmail.com>
2023-06-08 19:10:32 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pyrademacher",
-    version="0.9.12",
+    version="0.9.13",
     author="Pedro Ribeiro",
     author_email="pedroeusebio@gmail.com",
     description="Control devices connected to your Rademacher Homepilot "
